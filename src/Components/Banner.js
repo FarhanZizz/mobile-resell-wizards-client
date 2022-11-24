a1dc158a80311d6a2df129f967f0d12df53ab608
FarhanZizz <iamfarhanaziz@gmail.com>
2022-11-24 18:55:52 +0530

    changed banner image

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <section className="hero  mx-auto text-white bg-[url('/src/Assets/banner-bg.jpg')]">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img className='w-full' src={bannerImg} alt="" />
+                <img className="lg:w-1/2 rounded-lg" src={bannerImg} alt="" />
                 <div>
                     <h1 className="text-5xl leading-tight font-bold">The Largest Mobile Resell Marketplace in Bangladesh!</h1>
                     <p className="py-6">Sell Your Old Android/iOS Phone at <span className='font-bold'>MRW</span> & Get the Best Services, Quote & Free Pickup. Unlock the Best Market Price for Your Phone Now!</p>
