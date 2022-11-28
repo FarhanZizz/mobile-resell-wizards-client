bb68c6bc1e1c365522772fba6e3cc9eb6d991a1d
FarhanZizz <iamfarhanaziz@gmail.com>
2022-11-28 11:42:17 +0530

    added all buyers and sellers list with delete functionality

--- src/Pages/AllBuyers.js
+++ src/Pages/AllBuyers.js
@@ -1,9 +1,51 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
+import { BsFillTrashFill } from 'react-icons/bs';
 
 const AllBuyers = () => {
+    const { data: buyers = [], refetch } = useQuery({
+        queryKey: ['buyers'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/buyers`);
+            const data = await res.json();
+            return data
+        }
+    });
+    const handleDelete = (id) => {
+        fetch(`http://localhost:5000/user/delete/${id}`, { method: 'DELETE' })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount === 1) {
+                    refetch();
+                    toast.success('Deleted Successfully')
+                }
+            })
+    }
     return (
-        <div>
-            All buyers
+        <div className="overflow-x-auto rounded">
+            <h1 className="my-5 text-xl font-bold text-center">All Buyers</h1>
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {buyers.map((buyer, i) =>
+                        <tr key={buyer._id} className="hover">
+                            <th>{i + 1}</th>
+                            <td>{buyer.name}</td>
+                            <td>{buyer.email}</td>
+                            <td><button onClick={() => handleDelete(buyer._id)} className="btn btn-error border-0 font-semibold text-white"><BsFillTrashFill></BsFillTrashFill></button></td>
+                        </tr>
+
+                    )}
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/Pages/AllSellers.js
+++ src/Pages/AllSellers.js
@@ -1,11 +1,52 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
+import { BsFillTrashFill } from 'react-icons/bs';
 
 const AllSellers = () => {
+    const { data: sellers = [], refetch } = useQuery({
+        queryKey: ['sellers'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/sellers`);
+            const data = await res.json();
+            return data
+        }
+    });
+    const handleDelete = (id) => {
+        fetch(`http://localhost:5000/user/delete/${id}`, { method: 'DELETE' })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount === 1) {
+                    refetch();
+                    toast.success('Deleted Successfully')
+                }
+            })
+    }
     return (
-        <div>
-            all sellers
+        <div className="overflow-x-auto rounded">
+            <h1 className="my-5 text-xl font-bold text-center">All Buyers</h1>
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {sellers.map((seller, i) =>
+                        <tr key={seller._id} className="hover">
+                            <th>{i + 1}</th>
+                            <td>{seller.name}</td>
+                            <td>{seller.email}</td>
+                            <td><button onClick={() => handleDelete(seller._id)} className="btn btn-error border-0 font-semibold text-white"><BsFillTrashFill></BsFillTrashFill></button></td>
+                        </tr>
+
+                    )}
+                </tbody>
+            </table>
         </div>
     );
 };
-
-export default AllSellers;
+export default AllSellers
